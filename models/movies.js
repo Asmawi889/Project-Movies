5e3cbcaabd91287e2900b500c9522ac62bbe86d0
YUSUF <Yousifcool889@gmail.com>
2024-07-28 11:03:10 +0300

    making the server and connecting to the database

---
+++ models/movies.js
@@ -0,0 +1,20 @@
+
+const mongoose = require("mongoose");
+const { stringify } = require("querystring");
+
+
+
+
+const moviesSchema = new mongoose.Schema({
+  name: String,
+  Date: Number,
+  rating: String,
+  Description: String,
+  
+});
+
+const movie= mongoose.model("Movie", moviesSchema);
+
+
+
+module.exports = movie;
